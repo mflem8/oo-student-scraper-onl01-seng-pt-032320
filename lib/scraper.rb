72b6a41a6fdedbefcf047ef1724e276515aeac6d
Matt Fleming <matthewfleming8@gmail.com>
2020-05-05 01:23:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,6 +20,16 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
 
   end
 end
