151b5e78609addcff8c38c5dab47699d6f14bec1
Matt Fleming <matthewfleming8@gmail.com>
2020-05-05 01:28:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,11 @@
 require 'open-uri'
-require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     students = []
+    html = open(index_url)
     index_page.css("div.roster-cards-container"). each do |card|
       card.css(".student-card a"). each do |student|
         student_profile_link = "#{student.attr('href')}"
@@ -32,4 +32,6 @@ class Scraper
       end
     end
   end
+
+  student
 end
